043e38580bd0cf1208ceb35adfddf9ad995f7e82
Kenny Kane <55564615+ghsecurity@users.noreply.github.com>
2020-07-16 22:07:42 -0700

    Update TrafficBot.py

--- TrafficBot.py
+++ TrafficBot.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+
 import webbrowser
 import os
 import time
 
-url = '<url to visit>'
+url = 'https://onlyfans.com/?ref=13582078'
 
-tor_path="<path to Tor Browser>"
-webbrowser.register('tor', None, webbrowser.BackgroundBrowser(tor_path))
-webbrowser.get('tor').open(url)
-time.sleep(15)
-os.system("taskkill /im tor.exe /f")
-webbrowser.get('tor').open(url)
-os.system("taskkill /im tor.exe /f")
+def repeat():
+    tor_path = \
+        'C:\\Users\\etern\\OneDrive\\Desktop\\Tor Browser\\Browser\\firefox.exe'
+    webbrowser.register('tor', None, webbrowser.BackgroundBrowser(tor_path))
+    webbrowser.get('tor').open(url)
+    time.sleep(15)
+    os.system("taskkill /im tor.exe /f")
+
+while True:
+    repeat()
